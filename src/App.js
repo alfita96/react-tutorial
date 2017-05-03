c8be893d66593606bb4580e772eb07b26db463d9
Account for Alfonso Tanga <s1jllq@scl000001768.sccloud.swissre.com>
2017-05-03 11:11:52 +0200

    Deleted unneeded files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
-import Header from './structure/Header'
 import Navbar from './structure/Navbar';
 import Main from './structure/Main';
 import Gallery from './structure/Gallery';
@@ -10,25 +10,24 @@ import About from './structure/About';
 import NoMatch from './NoMatch';
 
 class App extends Component {
-	  render() {
+  render() {
     return (
-    	<div className="container">
-				<Header />
-				
-				<Router>
-					<div>
-						<Navbar />
-						<Switch>
-							<Route exact path="/" component={Main}/>
-							<Route path="/gallery" component={Gallery}/>
-							<Route path="/location" component={Location}/>
-							<Route path="/about" component={About}/>
-							<Route component={NoMatch}/>
-						</Switch>
-					</div>
-				</Router>
-				
-			</div>
+      <MuiThemeProvider>
+        <div className="container">
+          <Router>
+            <div>
+              <Navbar />
+              <Switch>
+                <Route exact path="/" component={Main}/>
+                <Route path="/gallery" component={Gallery}/>
+                <Route path="/location" component={Location}/>
+                <Route path="/about" component={About}/>
+                <Route component={NoMatch}/>
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
